cf46e0e66a63a87758d6966d1fc2e9a906eaf011
dhengGhuring <wyvrendragon@gmail.com>
2022-02-11 02:12:20 +0700

    update

--- practice1.js
+++ practice1.js
@@ -3,21 +3,28 @@ console.log("==========================");
 console.log("Show List Of Stock Items");
 console.log("==========================");
 
-let stationary = {
-  nama: "Pencil",
-  stock: 20,
+let items = {
+  stationary: {
+    nameItem: "Pencil",
+    stock: 20,
+  },
+  electronic: {
+    nameItem: "Mouse",
+    stock: 10,
+  },
 };
 
 // console.log(`Stock item Stationary ${stationary.nama} = ${stationary.stock}`);
 
-const stock = (name, stock) => {
+const stock = (nameItems, name, stock) => {
   if (stock == 0) {
-    return `Stock item Stationary = ${name} was empty!`;
-  } else if (stock < 5) {
-    return `Stock item Stationary = ${name} less!`;
+    // return `Stock item ${items.stationary.nameItem} = ${name} was empty!`;
+    return `Stock item ${nameItems} = ${name} was empty!`;
+  } else if (stock <= 5) {
+    return `Stock item ${nameItems} = ${name} less!`;
   } else {
-    return `Stock item Stationary = ${name} still many`;
+    return `Stock item ${nameItems} = ${name} still many`;
   }
 };
-let checkStock = stock("Pencil", 0);
+let checkStock = stock("Electronic", "Mouse", 5);
 console.log(checkStock);
